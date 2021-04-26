824286afde714b0dc1b1cf0acdc1d5071c024681
stevending1st <1139274654@qq.com>
2021-04-26 11:34:03 +0800

    deploy

--- volunteerms-fe/vue-config.js
+++ volunteerms-fe/vue-config.js
@@ -1,8 +1,35 @@
+const cdn = {
+  css: [],
+  js: [
+    "https://cdn.bootcss.com/vue/2.5.17/vue.runtime.min.js",
+    "https://cdn.bootcss.com/vue-router/3.0.1/vue-router.min.js",
+    "https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js",
+    "https://cdn.bootcss.com/axios/0.18.0/axios.min.js"
+  ]
+};
+
 module.exports = {
+  chainWebpack: config => {
+    // 生产环境配置
+    if (process.env.NODE_ENV === "ghpage") {
+      // 生产环境注入cdn
+      config.plugin("html").tap(args => {
+        args[0].cdn = cdn;
+        return args;
+      });
+    }
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV === "ghpage") {
       config.mode = "production";
       config.devtool = "nosources-source-map";
+      // 用cdn方式引入
+      config.externals = {
+        vue: "Vue",
+        vuex: "Vuex",
+        "vue-router": "VueRouter",
+        axios: "axios"
+      };
     } else if (process.env.NODE_ENV === "dev") {
       config.mode = "development";
     } else {
@@ -32,5 +59,6 @@ module.exports = {
         SymbolId: "incon-[name]"
       });
   },
-  publicPath: process.env.VUE_APP_PUBLICPATH
+  publicPath: process.env.VUE_APP_PUBLICPATH,
+  productionSourceMap: process.env.NODE_ENV === "ghpage" ? false : true
 };
